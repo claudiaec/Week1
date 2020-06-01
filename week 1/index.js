55794d942fe46a104fa9805f59c4d6bab6234019
Claudia <claudia.escutia.arq@gmail.com>
2020-05-31 21:20:37 -0500

    Week 1 Commit

---
+++ week 1/index.js
@@ -0,0 +1,32 @@
+/*First Amount of variables*/
+var ipa=10;
+var wma=50;
+var friendsa=120;
+var agea=30;
+var firstn="Claudia ";
+var lan=" Cuevas";
+var middlen="E";
+
+
+/*Second Amount of variables*/
+var ipb=7;
+var wmb=60;
+var friendsb=150;
+var ageb=31;
+var firstn="Claudia ";
+var lan=" Cuevas";
+var middlen="E";
+
+/*New amount of money in wallet after buying the item*/
+console.log("my money left is  " + (wma-ipa));
+/*Number of friends made each year based on age*/
+console.log("My number of friends made  " + (friendsa/agea));
+/*Full name based on first name, middle initial and last name*/
+console.log("My full name is  " + (firstn+ middlen+ lan));
+
+/*New amount of money in wallet after buying the item*/
+console.log("my money left is  " + (wmb-ipb));
+/*Number of friends made each year based on age*/
+console.log("My number of friends made  " + (friendsb/ageb));
+/*Full name based on first name, middle initial and last name*/
+console.log("My full name is  " + (firstn+ middlen+ lan));
